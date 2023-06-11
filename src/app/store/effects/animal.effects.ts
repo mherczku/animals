1bb4672c0f0571864a6d496287937e77b7f926b0
Herczku Máté <mherczku@gmail.com>
2023-06-11 22:11:41 +0200

    effects add timeout, loader, takeuntil and make pure failure

--- src/app/store/effects/animal.effects.ts
+++ src/app/store/effects/animal.effects.ts
@@ -1,6 +1,6 @@
 import {Injectable} from "@angular/core";
 import {Actions, createEffect, ofType} from "@ngrx/effects";
-import {catchError, exhaustMap, map, of, tap, TimeoutError} from "rxjs";
+import {catchError, exhaustMap, finalize, map, of, takeUntil, tap, timeout, TimeoutError} from "rxjs";
 import * as actions from '../actions/animal.actions';
 import {HotToastService} from "@ngneat/hot-toast";
 import {AnimalService} from "../../service/animal/animal.service";
@@ -19,17 +19,30 @@ export class AnimalEffects {
     this.actions$.pipe(
       ofType(actions.animalLoad),
 
-
+      // Hot Toast Loader:
+      tap(action => {
+        this.toast.loading(`Loading ${action.animalType}...`, {id: "dataLoader"});
+      }),
       exhaustMap(action =>
         this.animalService.getAnimals(action.animalType).pipe(
+          // timeout if request takes more than 3 seconds
+          timeout(3000),
 
           map(response => {
             console.log(`animal ${action.animalType} response:::`, response)
             return actions.animalLoadSuccess({page: action.animalType, animals: response})
           }),
 
+          // Close loader when done
+          finalize(() => this.toast.close("dataLoader")),
+
+          // to prevent memory leaks
+          takeUntil(this.actions$.pipe(
+            ofType(actions.animalLoadCancel)
+          )),
+
           catchError((error: any) => {
-            if(error instanceof TimeoutError) {
+            if (error instanceof TimeoutError) {
               return of(actions.animalLoadFailure({errorMessage: "Network Timeout"}))
             }
             return of(actions.animalLoadFailure({errorMessage: error.toString()}))
@@ -45,7 +58,9 @@ export class AnimalEffects {
         console.error(failure.errorMessage)
         this.toast.error(failure.errorMessage)
       })
-    )
+
+      // pure side effect:
+    ), {dispatch: false}
   );
 
 }
