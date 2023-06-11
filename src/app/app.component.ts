c79fc8ba3586618e2f37964366893b5f9f74cab1
Herczku Máté <mherczku@gmail.com>
2023-06-11 22:06:58 +0200

    add route animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {ChildrenOutletContexts} from "@angular/router";
+import {routeAnimations} from "./ui/animations/route.animation";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [routeAnimations],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
-  title = 'animals';
+  title = 'Animals';
+
+  constructor(private contexts: ChildrenOutletContexts) {
+  }
+
+  get getRouteAnimationData(): string {
+    return this.contexts.getContext("primary")?.route?.snapshot?.data?.["animation"];
+  }
 }
