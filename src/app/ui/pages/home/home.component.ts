7ad4729810d0360e67c12146b86d8404774b67db
Herczku Máté <mherczku@gmail.com>
2023-06-11 22:10:49 +0200

    home page cards select cursor

--- src/app/ui/pages/home/home.component.ts
+++ src/app/ui/pages/home/home.component.ts
@@ -1,17 +1,24 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
 import {AnimalCardComponent} from "../../components/animal-card/animal-card.component";
 import {RouterLink} from "@angular/router";
 import {Animal} from "../../../interface/animal";
+import {CatRestrictionService} from "../../../service/cat-restriction/cat-restriction.service";
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [CommonModule, AnimalCardComponent, RouterLink],
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HomeComponent {
+
+  isCatAvailable$ = this.catService.watchCatAvailable();
+
+  constructor(private readonly catService: CatRestrictionService) {
+  }
 
   animalTypes: Animal[] = [
     {
