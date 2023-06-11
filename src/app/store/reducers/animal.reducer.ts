d146caa9c9a9eb4a6c0e9266124d160742278956
Herczku Máté <mherczku@gmail.com>
2023-06-11 19:40:17 +0200

    create animal reducer

---
+++ src/app/store/reducers/animal.reducer.ts
@@ -0,0 +1,29 @@
+import {createReducer, on} from "@ngrx/store";
+import {initAnimalState} from "../state/animal.state";
+import * as actions from "../actions/animal.actions"
+
+
+export const animalReducer = createReducer(
+  initAnimalState,
+  on(actions.animalLoad, (state, {animalType}) => (
+    {
+      ...state,
+      page: animalType,
+    }
+  )),
+
+  on(actions.animalLoadSuccess, (state, {animals, page}) => (
+    {
+      ...state,
+      animals: animals,
+      page: page,
+    }
+  )),
+
+  on(actions.animalLoadFailure, (state, {errorMessage}) => (
+    {
+      ...state,
+      animals: [],
+    }
+  ))
+)
